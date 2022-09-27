2eb894bdd3384e524bf7d6daf94b35a462ad1732
danman0521 <jdmartinez.p@misena.edu.co>
2022-09-27 13:48:45 -0500

    Update ClienteService.java

--- Spring/Ciclo3/src/main/java/com/MinTicCiclo3/Service/ClienteService.java
+++ Spring/Ciclo3/src/main/java/com/MinTicCiclo3/Service/ClienteService.java
@@ -1,5 +1,39 @@
 package com.MinTicCiclo3.Service;
 
+import com.MinTicCiclo3.Model.Cliente;
+import com.MinTicCiclo3.Repository.ClienteRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
 public class ClienteService {
     
+    @Autowired
+    private ClienteRepository  clienteRepository;
+    
+    public List<Cliente> obtenerClienteCompleta(){
+        return clienteRepository.obtenerClienteCompleta();
+    }
+    
+    public Optional <Cliente> obtenerClienteId(Integer id){
+        return clienteRepository.obtenerClienteId(id);
+    }
+    
+    public  Cliente salvarCliente(Cliente cliente){
+        if (cliente.getId()== null){
+           return clienteRepository.salvarCliente(cliente);
+        }else{
+         Optional<Cliente> clienteAuxiliar = clienteRepository.obtenerClienteId(cliente.getId());
+            if(clienteAuxiliar.isEmpty()){
+                return clienteRepository.salvarCliente(cliente);
+        }else{
+               return cliente; 
+                }
+    
+    }
+}
 }
