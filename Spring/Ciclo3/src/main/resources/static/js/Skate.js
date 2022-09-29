296fea4a1426194b45cf4d51b0c104782902df3c
Anderson Fabian Chipatecua Jimenez <andersonf0615@gmail.com>
2022-09-29 16:17:44 -0500

    index y js

--- Spring/Ciclo3/src/main/resources/static/js/Skate.js
+++ Spring/Ciclo3/src/main/resources/static/js/Skate.js
@@ -1,12 +1,15 @@
-function InsertarInformacionCategory() {
+function InsertarInformacionSkates() {
     $("#resultado").empty();
     let myData = {
-        name: $("#nameCategory").val(),
-        description: $("#descriptionCategory").val(),
+        name: $("#nameSkate").val(),
+        description: $("#descriptionSkate").val(),
+        brand: $("#brandSkate").val(),
+        yearb:$("#yearSkate").val(),
+        categoryId :$("#category_idSkate").val(),
     };
     let dataToSend = JSON.stringify(myData);
     $.ajax({
-        url: "http://localhost:8080/api/Category/save",
+        url: "http://localhost:8080/api/Skate/save",
         type: "POST",
         data: dataToSend,
         datatype: "JSON",
@@ -21,9 +24,9 @@ function InsertarInformacionCategory() {
     );
 }
 
-function ConsultarInformacionCategory() {
+function ConsultarInformacionSkates() {
     $.ajax({
-        url: "http://localhost:8080/api/Category/all",
+        url: "http://localhost:8080/api/Skate/all",
         type: "GET",
         datatype: "JSON",
         success: function (respuesta) {
@@ -36,16 +39,23 @@ function ConsultarInformacionCategory() {
     );
 }
 
-function pintarRespuestaCategory(items) {
+function pintarRespuestaSkates(items) {
     $("#resultado").empty();
     let myTable = "<table border='1'>";
     myTable += "<caption> SKATE </caption>";
-    myTable += "<tr><th>CODE</th> <th> NAME</th></th>";
+    myTable += "<tr><th>CODE</th> <th> NAME</th> <th> BRAND</th> <th> YEAR</th> </th>";
     for (i = 0; i < items.length; i++) {
         myTable += "<tr>";
         myTable += "<td>" + items[i].id + "</td>";
         myTable += "<td>" + items[i].name + "</td>";
+        myTable += "<td>" + items[i].brand + "</td>";
+        myTable += "<td>" + items[i].yearb + "</td>";
         myTable += "<td>" + items[i].description + "</td>";
+        myTable += "<td>" + items[i].categoryId + "</td>";
+        myTable += "<td>" + items[i].category.name + "</td>";
+        myTable += "<td>" + items[i].category.description + "</td>";
+        myTable += "<td>" + items[i].message + "</td>";
+        myTable += "<td>" + items[i].reservation + "</td>";
         //myTable += "<td> <button onclick='borrarElementoCategory(" + items[i].id + ")'> Borrar </button>";
         myTable += "</tr>";
     }
