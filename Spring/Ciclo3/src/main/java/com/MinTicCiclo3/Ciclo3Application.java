bb9cd8a6c8238cdbfefe1b5681ff85c8b2c4391a
danman0521 <jdmartinez.p@misena.edu.co>
2022-09-30 13:30:42 -0500

    Update Ciclo3Application.java

--- Spring/Ciclo3/src/main/java/com/MinTicCiclo3/Ciclo3Application.java
+++ Spring/Ciclo3/src/main/java/com/MinTicCiclo3/Ciclo3Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan(basePackages = { "com.MinTicCiclo3.model" })
+@EntityScan(basePackages = {"com.MinTicCiclo3.model" })
 @SpringBootApplication
 public class Ciclo3Application {
 
