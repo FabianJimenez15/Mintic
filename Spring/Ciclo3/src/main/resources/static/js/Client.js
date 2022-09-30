c7c05bdb990150ca0cbd706d923ef23b6c86ee4a
danman0521 <jdmartinez.p@misena.edu.co>
2022-09-29 21:06:12 -0500

    Create Client.js

---
+++ Spring/Ciclo3/src/main/resources/static/js/Client.js
@@ -0,0 +1,55 @@
+function InsertarInformacionClient(){
+   $("#resultadoCliente").empty();
+   let myData={ 
+       name:$("#nameCliente").val(),
+       email:$("#emailCliente").val(),
+       password:$("#passwordCliente").val(),
+       age:$("#ageCliente").val(),
+   };
+    let dataToSend =JSON.stringify(myData);
+    $.ajax({
+        url: "http://localhost:8080/api/Client/save",
+        type: "POST",
+        data: dataToSend,
+        datatype: "JSON",
+        contentType: 'application/json',
+         success: function (respuesta) {
+            alert("Insercion exitosa");
+        },
+        error: function (xhr, status) {
+            alert('Operacion no satisfactoria' + xhr.status);
+        }
+    });
+}
+function ConsultarInformacionClient(){
+    $.ajax({
+    url: "http://localhost:8080/api/Client/all",
+         type : "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
+            pintarRespuestaSkates(respuesta);
+        },
+        error: function (xhr, status) {
+            alert('Operacion no satisfactoria' + xhr.status);
+        }
+        });
+}
+function pintarRespuestaSkates(items) {
+    $("#resultadoCliente").empty();
+    let myTable =  "<table border='1'>";
+     myTable += "<caption> CLIENT <caption>";
+     myTable += "<tr> <th>ID </th> <th> EMAIL </th>  <th>PASSWORD </th>  <th>NAME </th> <th>AGE </th> <th>MESSAGE</th> <th>RESERVATION </th>  </TF>";
+     for (i=0; i < items.length; i++){
+          myTable+= "<tr>";
+          myTable += "<td>" + items[i].id + "</td>";
+          myTable += "<td>" + items[i].email + "</td>";
+          myTable += "<td>" + items[i].password + "</td>";
+          myTable += "<td>" + items[i].name + "</td>";
+          myTable += "<td>" + items[i].age + "</td>";
+          myTable += "<td>" + items[i].message + "</td>";
+          myTable += "<td>" + items[i].reservation + "</td>";
+          
+     }
+     myTable += "</table>";
+     $("#resultado").append(myTable);
+}
